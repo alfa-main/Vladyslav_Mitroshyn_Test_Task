dcf9d53a1825d7e748a59561c51372cdbec0d4e8
Vladyslav Mitroshyn <metr.2017.rail.com@gmail.com>
2024-12-14 12:22:21 +0200

    feat: add generic button component

--- mitroshyn_test_task/src/App.tsx
+++ mitroshyn_test_task/src/App.tsx
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
+import Button from "./components/atoms/Button/Button";
 
 function App() {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-900 text-white">
-      <h1 className="text-3xl font-bold">Tailwind CSS works!</h1>
+      <Button variant="primary" size="medium">
+        Bet Now
+      </Button>
     </div>
   );
 }
